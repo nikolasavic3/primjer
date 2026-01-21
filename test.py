969e0161af7d1480899ead78a3d9cd47178e0853
Nikola Savic <nikola.savic20@gmail.com>
2026-01-21 11:21:03 +0100

    cv

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pickle
 import numpy as np
+import cv2
+
 
 # Load model - THIS CAN FAIL!
 with open('model.pkl', 'rb') as f:
@@ -7,3 +9,7 @@ with open('model.pkl', 'rb') as f:
 
 sample = np.array([[0.5, -0.2, 0.1, 0.8]], dtype=np.float)
 print(f"Prediction: {model.predict(sample)}")
+
+cap = cv2.VideoCapture(0)  # or a video file
+print(f"OpenCV version: {cv2.__version__}")
+print(f"Camera opened: {cap.isOpened()}")
